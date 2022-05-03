3e091893eee78c81b36365cc2ad740e6c143b806
vasgat <vasgat@iti.gr>
2022-05-03 16:54:30 +0200

    minor edits

--- src/app/models/filter.model.ts
+++ src/app/models/filter.model.ts
@@ -1,4 +1,6 @@
+import {ValueRange} from "./valuerange.model";
+
 export class Filter {
-  constructor(public name: string, public value: string | number | any[]) {
+  constructor(public name: string, public value: string | number | any[] | ValueRange) {
   }
 }
