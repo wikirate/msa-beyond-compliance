6c1439fb4e211c90c51e2025baff6bd4bc862ae0
vasgat <vasgat@iti.gr>
2022-05-10 10:21:56 +0200

    fixing calculation of disclosure rates

--- src/app/dashboard/disclosure-rates/disclosure-rates.component.ts
+++ src/app/dashboard/disclosure-rates/disclosure-rates.component.ts
@@ -26,118 +26,66 @@ export class DisclosureRatesComponent implements OnInit {
   updateDisclosureRates() {
     this.isLoading = true;
     this.dataProvider.getAnswers(
-      this.dataProvider.metrics.uk_msa_statement_assessed, [
+      this.dataProvider.metrics.msa_statement_assessed, [
         new Filter("year", this.year),
         new Filter('value', 'Yes'),
         new Filter("company_group", "MSA Garment")
       ]
-    ).subscribe(uk_assessed => {
-      let uk_msa_statements_assessed = uk_assessed.map((a: { [x: string]: any; }) => {
-        return {company: a['company'], year: a['year']}
+    ).subscribe(assessed => {
+      this.dataProvider.getAnswers(
+        this.dataProvider.metrics.msa_disclosure_rate, [
+          new Filter("year", this.year),
+          new Filter("company_group", "MSA Garment")
+        ]
+      ).subscribe(disclosure_rates => {
+        this.garment_avg_disclosure_rate = 0;
+        disclosure_rates = disclosure_rates.filter((a: { company: any; year: any; }) => assessed.some((statement: { company: any; year: any; }) => statement.company == a.company && statement.year == a.year))
+        for (let answer of disclosure_rates) {
+          this.garment_avg_disclosure_rate += Number(answer['value']);
+        }
+        this.garment_avg_disclosure_rate = Math.round(this.garment_avg_disclosure_rate / disclosure_rates.length)
+        this.dataProvider.getAnswers(
+          this.dataProvider.metrics.msa_statement_assessed, [
+            new Filter("year", this.year),
+            new Filter('value', 'Yes'),
+            new Filter("company_group", "MSA Financial")
+          ]
+        ).subscribe(assessed => {
+          this.dataProvider.getAnswers(
+            this.dataProvider.metrics.msa_disclosure_rate, [
+              new Filter("year", this.year),
+              new Filter("company_group", "MSA Financial")
+            ]
+          ).subscribe(disclosure_rates => {
+            this.financial_avg_disclosure_rate = 0;
+            disclosure_rates = disclosure_rates.filter((a: { company: any; year: any; }) => assessed.some((statement: { company: any; year: any; }) => statement.company == a.company && statement.year == a.year))
+            for (let answer of disclosure_rates) {
+              this.financial_avg_disclosure_rate += Number(answer['value']);
+            }
+            this.financial_avg_disclosure_rate = Math.round(this.financial_avg_disclosure_rate / disclosure_rates.length)
+          })
+        })
       })
       this.dataProvider.getAnswers(
-        this.dataProvider.metrics.aus_msa_statement_assessed, [new Filter("year", this.year), new Filter('value', 'Yes'),
-          new Filter("company_group", "MSA Garment")]
-      ).subscribe(aus_assessed => {
-        let aus_msa_statements_assessed: any[] = aus_assessed.map((a: { [x: string]: any; }) => {
-          return {company: a['company'], year: a['year']}
-        })
-        let total_assessed = [...new Set([...uk_msa_statements_assessed, ...aus_msa_statements_assessed])];
-        total_assessed = total_assessed.filter((arr, index, self) =>
-          index === self.findIndex((t) => (t.company === arr.company && t.year === arr.year)))
+        this.dataProvider.metrics.msa_statement_assessed, [
+          new Filter("year", this.year),
+          new Filter('value', 'Yes'),
+          new Filter("company_group", "MSA Hospitality")
+        ]
+      ).subscribe(assessed => {
         this.dataProvider.getAnswers(
           this.dataProvider.metrics.msa_disclosure_rate, [
             new Filter("year", this.year),
-            new Filter("company_group", "MSA Garment")
+            new Filter("company_group", "MSA Hospitality")
           ]
         ).subscribe(disclosure_rates => {
-          this.garment_avg_disclosure_rate = 0;
+          this.hospitality_avg_disclosure_rate = 0;
+          disclosure_rates = disclosure_rates.filter((a: { company: any; year: any; }) => assessed.some((statement: { company: any; year: any; }) => statement.company == a.company && statement.year == a.year))
           for (let answer of disclosure_rates) {
-            if (total_assessed.find((item: { company: any; year: any; }) => {
-              return item['company'] == answer['company'] && item['year'] == answer['year']
-            }) !== undefined) {
-              this.garment_avg_disclosure_rate += Number(answer['value']);
-            }
+            this.hospitality_avg_disclosure_rate += Number(answer['value']);
           }
-          this.garment_avg_disclosure_rate = Math.round(this.garment_avg_disclosure_rate / total_assessed.length)
-          this.dataProvider.getAnswers(
-            this.dataProvider.metrics.uk_msa_statement_assessed, [
-              new Filter("year", this.year),
-              new Filter('value', 'Yes'),
-              new Filter("company_group", "MSA Financial")
-            ]
-          ).subscribe(uk_assessed => {
-            let uk_msa_statements_assessed = uk_assessed.map((a: { [x: string]: any; }) => {
-              return {company: a['company'], year: a['year']}
-            })
-            this.dataProvider.getAnswers(
-              this.dataProvider.metrics.aus_msa_statement_assessed, [new Filter("year", this.year), new Filter('value', 'Yes'),
-                new Filter("company_group", "MSA Financial")]
-            ).subscribe(aus_assessed => {
-              let aus_msa_statements_assessed: any[] = aus_assessed.map((a: { [x: string]: any; }) => {
-                return {company: a['company'], year: a['year']}
-              })
-              let total_assessed = [...new Set([...uk_msa_statements_assessed, ...aus_msa_statements_assessed])];
-              total_assessed = total_assessed.filter((arr, index, self) =>
-                index === self.findIndex((t) => (t.company === arr.company && t.year === arr.year)))
-              this.dataProvider.getAnswers(
-                this.dataProvider.metrics.msa_disclosure_rate, [
-                  new Filter("year", this.year),
-                  new Filter("company_group", "MSA Financial")
-                ]
-              ).subscribe(disclosure_rates => {
-                this.financial_avg_disclosure_rate = 0;
-                for (let answer of disclosure_rates) {
-                  if (total_assessed.find((item: { company: any; year: any; }) => {
-                    return item['company'] == answer['company'] && item['year'] == answer['year']
-                  }) !== undefined) {
-                    this.financial_avg_disclosure_rate += Number(answer['value']);
-                  }
-                }
-                this.financial_avg_disclosure_rate = Math.round(this.financial_avg_disclosure_rate / total_assessed.length)
-              })
-            })
-          })
-          this.dataProvider.getAnswers(
-            this.dataProvider.metrics.uk_msa_statement_assessed, [
-              new Filter("year", this.year),
-              new Filter('value', 'Yes'),
-              new Filter("company_group", "MSA Hospitality")
-            ]
-          ).subscribe(uk_assessed => {
-            let uk_msa_statements_assessed = uk_assessed.map((a: { [x: string]: any; }) => {
-              return {company: a['company'], year: a['year']}
-            })
-            this.dataProvider.getAnswers(
-              this.dataProvider.metrics.aus_msa_statement_assessed, [new Filter("year", this.year), new Filter('value', 'Yes'),
-                new Filter("company_group", "MSA Hospitality")]
-            ).subscribe(aus_assessed => {
-              let aus_msa_statements_assessed: any[] = aus_assessed.map((a: { [x: string]: any; }) => {
-                return {company: a['company'], year: a['year']}
-              })
-              let total_assessed = [...new Set([...uk_msa_statements_assessed, ...aus_msa_statements_assessed])];
-              total_assessed = total_assessed.filter((arr, index, self) =>
-                index === self.findIndex((t) => (t.company === arr.company && t.year === arr.year)))
-              this.dataProvider.getAnswers(
-                this.dataProvider.metrics.msa_disclosure_rate, [
-                  new Filter("year", this.year),
-                  new Filter("company_group", "MSA Hospitality")
-                ]
-              ).subscribe(disclosure_rates => {
-                this.hospitality_avg_disclosure_rate = 0;
-                for (let answer of disclosure_rates) {
-                  if (total_assessed.find((item: { company: any; year: any; }) => {
-                    return item['company'] == answer['company'] && item['year'] == answer['year']
-                  }) !== undefined) {
-                    this.hospitality_avg_disclosure_rate += Number(answer['value']);
-                  }
-                }
-                this.hospitality_avg_disclosure_rate = Math.round(this.hospitality_avg_disclosure_rate / total_assessed.length)
-              }, (error) => console.log(error), () => this.isLoading = false)
-            })
-          })
-        })
-
+          this.hospitality_avg_disclosure_rate = Math.round(this.hospitality_avg_disclosure_rate / disclosure_rates.length)
+        }, (error) => console.log(error), () => this.isLoading = false)
       })
     })
   }
