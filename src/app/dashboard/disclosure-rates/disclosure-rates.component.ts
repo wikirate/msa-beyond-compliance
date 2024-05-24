f29c0c56c4fd9119a20e018b2e26ed10dea42eab
Vasiliki Gkatziaki <vasgat@gmail.com>
2024-05-24 12:50:37 +0200

    add visualisations for disclosure-rates / two alternatives

--- src/app/dashboard/disclosure-rates/disclosure-rates.component.ts
+++ src/app/dashboard/disclosure-rates/disclosure-rates.component.ts
@@ -8,6 +8,7 @@ import {ActivatedRoute} from "@angular/router";
 import {ValueRange} from "../../models/valuerange.model";
 import {forkJoin} from "rxjs";
 import {WikirateUrlBuilder} from "../../utils/wikirate-url-builder";
+import { ChartsService } from 'src/app/services/charts.service';
 
 @Component({
   selector: 'disclosure-rates',
@@ -22,8 +23,9 @@ export class DisclosureRatesComponent implements OnInit {
   hospitality_avg_disclosure_rate: number = 0;
   food_bev_avg_disclosure_rate: number = 0;
   renewable_energy_avg_disclosure_rate: number = 0;
+  electronics_avg_disclosure_rate: number = 0;
 
-  constructor(private dataProvider: DataProvider, private sectorProvider: SectorProvider,
+  constructor(private dataProvider: DataProvider, private sectorProvider: SectorProvider, private chartsService: ChartsService,
               private route: ActivatedRoute) {
   }
 
@@ -77,9 +79,17 @@ export class DisclosureRatesComponent implements OnInit {
       ].filter(item => item.value != 'latest')
     )
 
+    const electronics_disclosure_rates = this.dataProvider.getAnswers(
+      this.dataProvider.metrics.msa_disclosure_rate, [
+        new Filter("year", this.year),
+        new Filter("company_group", this.dataProvider.company_groups.electronics)
+      ].filter(item => item.value != 'latest')
+    )
+
     forkJoin([food_and_bev_disclosure_rates,
       garment_disclosure_rates, financial_disclosure_rates,
-      hospitality_disclosure_rates, renewable_energy_disclosure_rates]).subscribe(results => {
+      hospitality_disclosure_rates, renewable_energy_disclosure_rates,
+      electronics_disclosure_rates]).subscribe(results => {
 
       if (this.year == 'latest') {
         results[0] = Object.values(results[0].reduce((r: any, o: any) => {
@@ -111,6 +121,12 @@ export class DisclosureRatesComponent implements OnInit {
 
           return r
         }, {}))
+
+        results[5] = Object.values(results[4].reduce((r: any, o: any) => {
+          r[o.company] = (r[o.company] && r[o.company].year > o.year) ? r[o.company] : o
+
+          return r
+        }, {}))
       }
 
       this.food_bev_avg_disclosure_rate = this.calc_avg_disclosure_rate(results[0]);
@@ -118,6 +134,21 @@ export class DisclosureRatesComponent implements OnInit {
       this.financial_avg_disclosure_rate = this.calc_avg_disclosure_rate(results[2]);
       this.hospitality_avg_disclosure_rate = this.calc_avg_disclosure_rate(results[3]);
       this.renewable_energy_avg_disclosure_rate = this.calc_avg_disclosure_rate(results[4]);
+      this.electronics_avg_disclosure_rate = this.calc_avg_disclosure_rate(results[5]);
+
+      this.drawSemiDonutChart(this.garment_avg_disclosure_rate, "semi-donut-disclosure-rates-garment", "#")
+      this.drawSemiDonutChart(this.food_bev_avg_disclosure_rate, "semi-donut-disclosure-rates-food-bev", "#")
+      this.drawSemiDonutChart(this.financial_avg_disclosure_rate, "semi-donut-disclosure-rates-finance", "#")
+      this.drawSemiDonutChart(this.hospitality_avg_disclosure_rate, "semi-donut-disclosure-rates-hospitality", "#")
+      this.drawSemiDonutChart(this.renewable_energy_avg_disclosure_rate, "semi-donut-disclosure-rates-energy", "#")
+      this.drawSemiDonutChart(this.electronics_avg_disclosure_rate, "semi-donut-disclosure-rates-electronics", "#")
+
+      this.drawSemiDonutChart(this.garment_avg_disclosure_rate, "semi-donut-disclosure-rates-garment-2", "#")
+      this.drawSemiDonutChart(this.food_bev_avg_disclosure_rate, "semi-donut-disclosure-rates-food-bev-2", "#")
+      this.drawSemiDonutChart(this.financial_avg_disclosure_rate, "semi-donut-disclosure-rates-finance-2", "#")
+      this.drawSemiDonutChart(this.hospitality_avg_disclosure_rate, "semi-donut-disclosure-rates-hospitality-2", "#")
+      this.drawSemiDonutChart(this.renewable_energy_avg_disclosure_rate, "semi-donut-disclosure-rates-energy-2", "#")
+      this.drawSemiDonutChart(this.electronics_avg_disclosure_rate, "semi-donut-disclosure-rates-electronics-2", "#")
 
       this.isLoading = false;
     })
@@ -133,7 +164,7 @@ export class DisclosureRatesComponent implements OnInit {
     let avg_discosure_rate = 0;
     for (let answer of disclosure_rates)
       avg_discosure_rate += answer['value'] == 'Unknown' ? 0 : Number(answer['value']);
-    return Math.round(avg_discosure_rate / disclosure_rates.length)
+    return Math.round(avg_discosure_rate*10 / disclosure_rates.length)
   }
 
   openURL(sector: string) {
@@ -144,5 +175,23 @@ export class DisclosureRatesComponent implements OnInit {
       .build();
 
     window.open(url, "_blank")
+  }
+
+  drawSemiDonutChart(percentage: number, elementId: string, url: string) {
+    this.chartsService.drawSemiDonutChart(
+      `${percentage}%`,
+      `div#${elementId}`,
+      [{
+        'id': 'Covered',
+        'percent': percentage,
+        'URL': url
+      },
+      {
+        'id': 'Not Covered',
+        'percent': 100 - percentage,
+        'URL': url
+      }],
+      250, 125, ["#FF5C45", "#E5E5EA"],
+      ["Covered", "Not Covered"], { renderer: "svg", actions: false })
   }
 }
