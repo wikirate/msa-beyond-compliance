9d04b43bfc339eb771e0ea4ef8a902ad652fcd2a
Vasiliki Gkatziaki <vasgat@gmail.com>
2024-05-28 15:03:19 +0200

    fixes on percentages calculations, change the incidents box content

--- src/app/dashboard/further-findings-alternative-two/further-findings.component.ts
+++ src/app/dashboard/further-findings-alternative-two/further-findings.component.ts
@@ -1,10 +1,10 @@
-import {Component, OnInit, TemplateRef, ViewChild} from '@angular/core';
-import {Filter} from "../../models/filter.model";
-import {DataProvider} from "../../services/data.provider";
-import {ActivatedRoute, ParamMap} from "@angular/router";
-import {SectorProvider} from "../../services/sector.provider";
-import {forkJoin} from "rxjs";
-import {NgbModal} from "@ng-bootstrap/ng-bootstrap";
+import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
+import { Filter } from "../../models/filter.model";
+import { DataProvider } from "../../services/data.provider";
+import { ActivatedRoute, ParamMap } from "@angular/router";
+import { SectorProvider } from "../../services/sector.provider";
+import { forkJoin } from "rxjs";
+import { NgbModal } from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
   selector: 'further-findings',
@@ -15,9 +15,9 @@ export class FurtherFindingsComponent implements OnInit {
   sector: string = "all-sectors";
   legislation: string = 'both';
   year: string = ''
-  percentage_of_companies_identified_incidents: number = 0;
-  percentage_of_companies_report_policy_beyond_t1: number = 0;
   percentage_of_companies_identified_risks: number = 0;
+  percentage_of_companies_report_policy_beyond_t1: number = 0;
+  percentage_of_no_remediations: number = 0;
   @ViewChild("content") content!: TemplateRef<any>;
 
   isLoading: boolean = true;
@@ -27,13 +27,13 @@ export class FurtherFindingsComponent implements OnInit {
 
   ngOnInit(): void {
     this.route.paramMap.subscribe((params: ParamMap) => {
-        let sector = params.get('sector');
-        if (sector !== null) {
-          this.sector = sector
-          this.updateData()
-        }
-        this.sectorProvider.getSector().next(sector);
+      let sector = params.get('sector');
+      if (sector !== null) {
+        this.sector = sector
+        this.updateData()
       }
+      this.sectorProvider.getSector().next(sector);
+    }
     )
     this.route.url.subscribe(val => {
       if (val[1].path === 'further-findings')
@@ -82,39 +82,38 @@ export class FurtherFindingsComponent implements OnInit {
 
     let assessed_statements = this.dataProvider.getAnswers(assessed_statements_metric_id, assessed_filters)
 
-    let incidents_identified = this.dataProvider.getAnswers(this.dataProvider.metrics.msa_incidents_identified,
+    let no_remediation = this.dataProvider.getAnswers(this.dataProvider.metrics.msa_incidents_remediation,
       [new Filter("year", this.year),
-        new Filter("value", ["Yes"]),
-        new Filter("company_group", company_group)])
+      new Filter("value", ["No", "In Development"]),
+      new Filter("company_group", company_group)])
 
     let risks_identified = this.dataProvider.getAnswers(this.dataProvider.metrics.msa_risks_identified,
       [new Filter("year", this.year),
-        new Filter("value", ["Yes"]),
-        new Filter("company_group", company_group)])
+      new Filter("value", ["Yes"]),
+      new Filter("company_group", company_group)])
 
     let policy_beyond_t1 = this.dataProvider.getAnswers(this.dataProvider.metrics.msa_policy_beyond_t1,
       [new Filter("year", this.year),
-        new Filter("value", ["Yes"]),
-        new Filter("company_group", company_group)])
+      new Filter("value", ["Yes"]),
+      new Filter("company_group", company_group)])
 
-    forkJoin([assessed_statements, incidents_identified, risks_identified, policy_beyond_t1]).subscribe(response => {
+    forkJoin([assessed_statements, no_remediation, risks_identified, policy_beyond_t1]).subscribe(response => {
       if (this.year == 'latest') {
         response[0] = Object.values(response[0].reduce((r: any, o: any) => {
           r[o.company] = (r[o.company] && r[o.company].year > o.year) ? r[o.company] : o
           return r
         }, {}))
-        response[1].filter((item: any) => response[0].find((o: any) => o.company == item.company && o.year == item.year))
-
-        response[2].filter((item: any) => response[0].find((o: any) => o.company == item.company && o.year == item.year))
-        response[3].filter((item: any) => response[0].find((o: any) => o.company == item.company && o.year == item.year))
       }
+      response[1] = response[1].filter((item: any) => response[0].find((o: any) => o.company == item.company && o.year == item.year))
+      response[2] = response[2].filter((item: any) => response[0].find((o: any) => o.company == item.company && o.year == item.year))
+      response[3] = response[3].filter((item: any) => response[0].find((o: any) => o.company == item.company && o.year == item.year))
 
       let assessed = response[0]
-      let incidents_identified = response[1]
+      let no_remediation = response[1]
       let reported_risks = response[2]
       let policy_beyond = response[3]
 
-      this.percentage_of_companies_identified_incidents = Math.round(incidents_identified.length * 100 / assessed.length);
+      this.percentage_of_no_remediations = Math.round(no_remediation.length * 100 / assessed.length);
       this.percentage_of_companies_identified_risks = Math.round(reported_risks.length * 100 / assessed.length);
       this.percentage_of_companies_report_policy_beyond_t1 = Math.round(policy_beyond.length * 100 / assessed.length);
     }, error => {
@@ -124,6 +123,6 @@ export class FurtherFindingsComponent implements OnInit {
   }
 
   openMessage() {
-    this.modalService.open(this.content, {centered: true});
+    this.modalService.open(this.content, { centered: true });
   }
 }
